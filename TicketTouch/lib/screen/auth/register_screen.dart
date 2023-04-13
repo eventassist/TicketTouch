3d6d966a0473b3621b10800c27287c8a053f496a
Olison Sturm <olisonsturm@gmx.de>
2023-04-13 22:16:54 +0200

    [+] codemagic.yaml

--- TicketTouch/lib/screen/auth/register_screen.dart
+++ TicketTouch/lib/screen/auth/register_screen.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:tickettouch/utils/helper_widgets.dart';
 
 class RegisterScreen extends StatefulWidget {
   final VoidCallback showLoginScreen;
